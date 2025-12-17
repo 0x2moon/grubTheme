67c913b7378fa3b5cf895d59aa2dfb424ea36574
0x2moon <0x2moon@protonmail.com>
2025-12-17 19:56:28 +0000

    correção

--- install_cyberexs.sh
+++ install_cyberexs.sh
@@ -44,6 +44,8 @@ fi
 echo "Updating GRUB..."
 if command -v update-grub >/dev/null 2>&1; then
     update-grub
+elif command -v grub-mkconfig >/dev/null 2>&1; then
+    grub-mkconfig -o /boot/grub/grub.cfg
 else
     echo "Error: Neither 'update-grub' nor 'grub-mkconfig' found. Please update GRUB manually."
     exit 1
